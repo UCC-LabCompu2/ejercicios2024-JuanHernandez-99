37db195c64f4d8190c49dc5e142bbcd51057d517
JuanHernandez-99 <juanhernandez0099@gmail.com>
2024-06-29 15:39:42 -0300

    ej setInterval

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -259,3 +259,26 @@ function dibujarImagen(posX,posY) {
     }
 
 }
+x=0;
+dx=2;
+function animarAuto(){
+    var canvas=document.getElementById("myCanvas");
+    var ctx=canvas.getContext("2d");
+    console.log(posX,posY);
+
+    canvas.width = canvas.width;
+
+    var img= new Image();
+    img.src= "images/auto.png";
+
+
+
+    img.onload= function (){
+        ctx.drawImage(img,x, 100);
+
+    }
+    if (x>canvas.width){
+        x=0;
+    }
+    x+=dx;
+}
