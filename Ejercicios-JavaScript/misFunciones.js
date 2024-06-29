793def4ac52dee27967a04e80e847ca73bcfe27b
JuanHernandez-99 <juanhernandez0099@gmail.com>
2024-06-29 14:34:17 -0300

    ej cuadriculado

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -186,3 +186,63 @@ function limpiarCanvas(){
     canvas.width=canvas.width;
 
 }
+function dibujarCuadriculado(){
+    var canvas= document.getElementById("myCanvas");
+    var ctx= canvas.getContext("2d");
+
+
+
+    var alturaMax=canvas.height;
+    var anchoMax=canvas.width;
+
+    //Dibujar lineas horizontales
+    ctx.beginPath();
+    for(var i=0;i<alturaMax;){
+
+        ctx.moveTo(0,i);
+
+        ctx.lineTo(anchoMax,i);
+        ctx.strokeStyle= "#3e67d9";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath()
+
+    //Dibujar lineas verticales
+    ctx.beginPath();
+    for(var i=0;i<anchoMax;){
+
+        ctx.moveTo(i,0);
+
+        ctx.lineTo(i,alturaMax);
+        ctx.strokeStyle= "#3e67d9";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath()
+
+
+//Eje X
+ctx.beginPath();
+
+ctx.moveTo(0,alturaMax/2);
+ctx.lineTo(anchoMax,alturaMax/2);
+ctx.strokeStyle= "#d91c00";
+ctx.stroke();
+
+ctx.closePath()
+
+//Eje Y
+
+    ctx.beginPath();
+
+    ctx.moveTo(anchoMax/2,0);
+    ctx.lineTo(anchoMax/2,alturaMax);
+    ctx.strokeStyle= "#d91c00";
+    ctx.stroke();
+
+    ctx.closePath()
+
+
+
+}
